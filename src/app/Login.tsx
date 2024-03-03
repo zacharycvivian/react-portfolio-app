732dde0a89c63ae9b5e0292b7d705c13b3cf266d
zacharycvivian <zacharycvivian@gmail.com>
2024-03-02 21:45:44 -0600

    Changed login page for mobile devices

--- src/app/Login.tsx
+++ src/app/Login.tsx
@@ -6,11 +6,11 @@ export default function Login() {
   return (
     <div className="login-page">
       <div className="login-container">
-        <h2>Choose a Login Method Below:</h2>
+        <h2>Zachary C. Vivian</h2>
         <button onClick={() => signIn("google", { callbackUrl: "/" })}>
           Login with Google
         </button>
-        <p className="login-footer">Zachary Vivian's Resume Website</p>
+        <p className="login-footer">Sign in to view my professional resume and testimonials.</p>
         <p className="disclaimer">
           Please note that authentication via Google is required to access this
           website in order to verify user identity.
