d9a0f3e6ecd0040c780d3c056e8be2d4108aaec1
zacharycvivian <zacharycvivian@gmail.com>
2024-03-03 09:23:02 -0600

    Added matrix background to login screen

--- src/app/Login.tsx
+++ src/app/Login.tsx
@@ -1,20 +1,139 @@
 "use client";
-import Image from 'next/image';
+import Image from "next/image";
 import { signIn } from "next-auth/react";
 import Logo from "@/../public/HeaderLogo.png";
 import "./Login.css";
+import { useEffect } from "react";
 
 export default function Login() {
+  useEffect(() => {
+    // Cast the elements to HTMLCanvasElement explicitly
+    var canvas = document.getElementById("canvas") as HTMLCanvasElement;
+    var ctx = canvas!.getContext("2d")!;
+    var canvas2 = document.getElementById("canvas2") as HTMLCanvasElement;
+    var ctx2 = canvas2!.getContext("2d")!;
+    var cw = window.innerWidth;
+    var ch = window.innerHeight;
+    var charArr = [
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+      "g",
+      "h",
+      "i",
+      "j",
+      "k",
+      "l",
+      "m",
+      "n",
+      "o",
+      "p",
+      "q",
+      "r",
+      "s",
+      "t",
+      "u",
+      "v",
+      "w",
+      "x",
+      "y",
+      "z",
+    ];
+    var maxCharCount = 100;
+    var fallingCharArr: Point[] = [];
+    var fontSize = 10;
+    var maxColumns = cw / fontSize;
+    canvas.width = canvas2.width = cw;
+    canvas.height = canvas2.height = ch;
+
+    function randomInt(min: number, max: number) {
+      return Math.floor(Math.random() * (max - min) + min);
+    }
+
+    function randomFloat(min: number, max: number) {
+      return Math.random() * (max - min) + min;
+    }
+
+    class Point {
+      x: number;
+      y: number;
+      value: string;
+      speed: number;
+
+      constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+        this.value = charArr[randomInt(0, charArr.length - 1)].toUpperCase();
+        this.speed = randomFloat(1, 5);
+      }
+
+      draw(ctx: CanvasRenderingContext2D) {
+        ctx2.fillStyle = "rgba(255,255,255,0.8)";
+        ctx2.font = fontSize + "px san-serif";
+        ctx2.fillText(this.value, this.x, this.y);
+
+        ctx.fillStyle = "#c6d6f6";
+        ctx.font = fontSize + "px san-serif";
+        ctx.fillText(this.value, this.x, this.y);
+
+        this.y += this.speed;
+        if (this.y > ch) {
+          this.y = randomFloat(-100, 0);
+          this.speed = randomFloat(2, 5);
+        }
+      }
+    }
+
+    for (var i = 0; i < maxColumns; i++) {
+      fallingCharArr.push(new Point(i * fontSize, randomFloat(-500, 0)));
+    }
+
+    let requestId: number;
+
+    var update = function () {
+      ctx.fillStyle = "rgba(0,0,0,0.05)";
+      ctx.fillRect(0, 0, cw, ch);
+
+      ctx2.clearRect(0, 0, cw, ch);
+
+      for (let i = fallingCharArr.length - 1; i >= 0; i--) {
+        fallingCharArr[i].draw(ctx);
+      }
+
+      requestId = requestAnimationFrame(update);
+    };
+
+    requestId = requestAnimationFrame(update);
+
+    // Clean up the animation frame when the component unmounts
+    return () => {
+      cancelAnimationFrame(requestId);
+    };
+  }, []);
+
   return (
     <div className="login-page">
+      <canvas id="canvas" className="matrix-canvas" />
+      <canvas id="canvas2" className="matrix-canvas" />
       <div className="login-container">
         {/* Use the Image component for the logo */}
-        <Image src={Logo} alt="Logo" className="login-logo" width={100} height={100} />
+        <Image
+          src={Logo}
+          alt="Logo"
+          className="login-logo"
+          width={100}
+          height={100}
+        />
         <h2>Zachary C. Vivian</h2>
         <button onClick={() => signIn("google", { callbackUrl: "/" })}>
           Login with Google
         </button>
-        <p className="login-footer">Sign in to view my professional resume and testimonials.</p>
+        <p className="login-footer">
+          Sign in to view my professional resume and testimonials.
+        </p>
         <p className="disclaimer">
           Please note that authentication via Google is required to access this
           website in order to verify user identity.
