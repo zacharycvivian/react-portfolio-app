566e20a1c9dea90d3d331329b32536161177f1f3
zacharycvivian <zacharycvivian@gmail.com>
2024-03-02 21:53:28 -0600

    Added logo to log in page

--- src/app/Login.tsx
+++ src/app/Login.tsx
@@ -1,11 +1,15 @@
 "use client";
+import Image from 'next/image';
 import { signIn } from "next-auth/react";
+import Logo from "@/../public/HeaderLogo.png";
 import "./Login.css";
 
 export default function Login() {
   return (
     <div className="login-page">
       <div className="login-container">
+        {/* Use the Image component for the logo */}
+        <Image src={Logo} alt="Logo" className="login-logo" width={100} height={100} />
         <h2>Zachary C. Vivian</h2>
         <button onClick={() => signIn("google", { callbackUrl: "/" })}>
           Login with Google
