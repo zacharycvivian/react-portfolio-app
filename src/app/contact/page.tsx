b196c4053c128d31ea3e98a2dc94ecf8079f0899
zacharycvivian <zacharycvivian@gmail.com>
2024-03-08 10:41:03 -0600

    Click cards to visit links instead of text, fixed apple motion

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 import React from "react";
 import styles from "./contact.module.css";
-import Link from 'next/link';
-
+import Link from "next/link";
 
 const downloadVCard = () => {
   const link = document.createElement("a");
@@ -14,70 +13,85 @@ const downloadVCard = () => {
 };
 
 const ContactPage = () => {
+  const handleExternalNavigation = (url: string) => {
+    window.open(url, "_blank", "noopener,noreferrer");
+  };
+  const handleMailTo = (emailAddress: string) => {
+    window.location.href = `mailto:${emailAddress}`;
+  };
+
+  const handleTel = (phoneNumber: string) => {
+    window.location.href = `tel:${phoneNumber}`;
+  };
   return (
     <div className={styles.container}>
       <Link href="/snake" legacyBehavior>
-      <div className={styles.apple}></div>
-    </Link>
-      <h2 className={styles.sectionTitle}>Contact Me:</h2>
+        <div className={styles.apple}></div>
+      </Link>
+      <h2 className={styles.sectionTitle}>
+        Contact Me:
+        <span className={styles.subtext}>Click a Card to Visit</span>
+      </h2>
       <div className={styles.section}>
-        <div className={styles.card}>
+        <div
+          className={styles.card}
+          onClick={() => handleMailTo("zacharycvivian@icloud.com")}
+        >
           <p>
-            <strong>Email:</strong>{" "}
-            <a href="mailto:zacharycvivian@icloud.com">
-              zacharycvivian@icloud.com
-            </a>
+            <strong>Email:</strong> zacharycvivian@icloud.com
             <br />
-            <span className={styles.subtext}>Personal Email for Job Opportunities Only</span>
+            <span className={styles.subtext}>
+              Personal Email for Job Opportunities Only
+            </span>
           </p>
         </div>
-        <div className={styles.card}>
+        <div className={styles.card} onClick={() => handleTel("+16085740816")}>
           <p>
-            <strong>Phone: </strong> <a href="tel:+16085740816">+1 608-574-0816</a>
+            <strong>Phone:</strong> +1 608-574-0816
             <br />
             <span className={styles.subtext}>Personal Cell Phone</span>
           </p>
         </div>
-        <div className={styles.card}>
+        <div
+          className={styles.card}
+          onClick={() =>
+            handleExternalNavigation(
+              "https://www.linkedin.com/in/zacharycvivian"
+            )
+          }
+        >
           <p>
-            <strong>LinkedIn: </strong>{" "}
-            <a
-              href="https://www.linkedin.com/in/zacharycvivian"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              zacharycvivian
-            </a>
+            <strong>LinkedIn: </strong>zacharycvivian
             <br />
             <span className={styles.subtext}>Connect With Me</span>
           </p>
         </div>
-        <div className={styles.card}>
+        <div
+          className={styles.card}
+          onClick={() =>
+            handleExternalNavigation("https://github.com/zacharycvivian")
+          }
+        >
           <p>
-            <strong>GitHub: </strong>{" "}
-            <a
-              href="https://github.com/zacharycvivian"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              zacharycvivian
-            </a>
+            <strong>GitHub:</strong> zacharycvivian
             <br />
             <span className={styles.subtext}>View My Repositories</span>
           </p>
         </div>
-        <div className={styles.card}>
+        <div
+          className={styles.card}
+          onClick={() =>
+            handleExternalNavigation(
+              "https://chat.openai.com/g/g-N5n358sXE-all-in-one-workflow-assistant"
+            )
+          }
+        >
           <p>
-            <strong>ChatGPT: </strong>{" "}
-            <a
-              href="https://chat.openai.com/g/g-N5n358sXE-all-in-one-workflow-assistant"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              All-in-One Workflow Assistant
-            </a>
+            <strong>ChatGPT:</strong> All-in-One Workflow Assistant
             <br />
-            <span className={styles.subtext}>My AI Assistant (Helped Me Make this Website!)</span>
+            <span className={styles.subtext}>
+              My AI Assistant (Helped Me Make this Website!)
+            </span>
           </p>
         </div>
       </div>
