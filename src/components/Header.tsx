24e874b58751c2c43ec552d065357de96e203bd2
zacharycvivian <zacharycvivian@gmail.com>
2024-03-12 19:45:57 -0500

    Added profile image for user sign in

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,30 +2,52 @@
 import React from "react";
 import styles from "./Header.module.css";
 import { useSession, signIn, signOut } from "next-auth/react";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import DefaultImage from "@/../public/defaultavatar.jpg";
+import Link from "next/link";
 
 function Header() {
   const { data: session } = useSession();
 
   const handleAuthAction = () => {
     if (session) {
-      // User is logged in, handle logout
       signOut({ callbackUrl: "/" });
     } else {
-      // User is not logged in, redirect to login page
-      // Replace '/api/auth/signin' with your login route if it's different
-      signIn('google', { callbackUrl: "/" });
+      signIn("google", { callbackUrl: "/" });
     }
   };
+
+  // Determine the image to use for the DropdownMenuTrigger
+  const userImageURL = session?.user?.image || DefaultImage.src;
 
   return (
     <header className={styles.header}>
       <h2>Zachary Vivian</h2>
-      <button
-        onClick={handleAuthAction}
-        className={session ? styles.logoutButton : styles.loginButton}
-      >
-        {session ? "Logout" : "Login"}
-      </button>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <button className={styles.iconButton}>
+            <img
+              src={userImageURL}
+              alt="Profile"
+              className="h-[1.2rem] w-[1.2rem] rounded-full" // Adjust styles as needed
+            />
+          </button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className={styles.dropdownContent}>
+          <DropdownMenuItem onClick={handleAuthAction}>
+            {session ? "Logout" : "Google Login"}
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Link href="/edit-profile">Edit Profile</Link>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </header>
   );
 }
