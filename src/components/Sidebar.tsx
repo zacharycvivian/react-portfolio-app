d1c8128014ae7fef18ccbd67db06c64d02c4ae02
Zachary Vivian <161660633+zacharycvivian@users.noreply.github.com>
2024-03-30 10:41:58 -0500

    Fixed sidebar button

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -35,7 +35,7 @@ function Sidebar() {
         >
           <svg
             width="40"
-            height="80"
+            height="40"
             viewBox="0 0 15 15"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
