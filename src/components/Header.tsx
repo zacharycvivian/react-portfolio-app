f09cb6c11b13e3dd3da3006278bc6027b8ae6baa
Zachary Vivian <161660633+zacharycvivian@users.noreply.github.com>
2024-03-29 21:18:04 -0500

    Added database to bugs and feedback

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,6 @@
 import React, { useState } from "react";
 import styles from "./Header.module.css";
 import { useSession, signIn, signOut } from "next-auth/react";
-import { ModeToggle } from "@/components/ModeToggle";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -15,6 +14,23 @@ import Link from "next/link";
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import { useTheme } from "next-themes";
 import ReactDOM from "react-dom";
+import { db } from "@/../firebase";
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
+
+//Interfaces for type declarations
+interface Feedback {
+  id: string;
+  email: string;
+  feedback: string;
+  time: any;
+}
+
+interface Bugs {
+  id: string;
+  email: string;
+  bugs: string;
+  time: any;
+}
 
 interface ModalProps {
   isOpen: boolean;
@@ -45,6 +61,22 @@ function Header() {
       </div>,
       document.body
     );
+  };
+
+  const addFeedback = async (email: string, feedback: string) => {
+    await addDoc(collection(db, "feedback"), {
+      email,
+      feedback,
+      time: serverTimestamp(),
+    });
+  };
+
+  const addBugReport = async (email: string, bugDescription: string) => {
+    await addDoc(collection(db, "bugs"), {
+      email,
+      bugs: bugDescription,
+      time: serverTimestamp(),
+    });
   };
 
   const handleAuthAction = () => {
@@ -131,38 +163,39 @@ function Header() {
           </DropdownMenuContent>
         </DropdownMenu>
         <Modal
-          isOpen={isReportBugModalOpen}
-          onClose={() => setReportBugModalOpen(false)}
-          title="Report Bugs"
-        >
-          <form>
-            <textarea
-              className={styles.textareaField}
-              placeholder="Describe the bug..."
-            ></textarea>
-            <div className={styles.formButtons}>
-              <button type="submit" className={styles.submitButton}>
-                Submit
-              </button>
-              <button
-                type="button"
-                className={styles.cancelButton}
-                onClick={() => setReportBugModalOpen(false)}
-              >
-                Cancel
-              </button>
-            </div>
-          </form>
-        </Modal>
-        <Modal
           isOpen={isFeedbackModalOpen}
           onClose={() => setFeedbackModalOpen(false)}
           title="Give Feedback"
         >
-          <form>
+          <form
+            onSubmit={async (e) => {
+              e.preventDefault();
+              // Correctly type the event target to access form elements
+              const form = e.target as HTMLFormElement;
+              const feedback = form.elements.namedItem(
+                "feedback"
+              ) as HTMLTextAreaElement;
+
+              // Check for session and session.user.email existence
+              if (
+                session &&
+                session.user &&
+                session.user.email &&
+                feedback &&
+                feedback.value
+              ) {
+                await addFeedback(session.user.email, feedback.value);
+                alert("Feedback submitted successfully!");
+                setFeedbackModalOpen(false); // Close the modal
+              } else {
+                alert("An unexpected error occurred."); // Handle unexpected case
+              }
+            }}
+          >
             <textarea
               className={styles.textareaField}
               placeholder="Your feedback..."
+              name="feedback"
             ></textarea>
             <div className={styles.formButtons}>
               <button type="submit" className={styles.submitButton}>
@@ -172,6 +205,58 @@ function Header() {
                 type="button"
                 className={styles.cancelButton}
                 onClick={() => setFeedbackModalOpen(false)}
+              >
+                Cancel
+              </button>
+            </div>
+          </form>
+        </Modal>
+        <Modal
+          isOpen={isReportBugModalOpen}
+          onClose={() => setReportBugModalOpen(false)}
+          title="Report Bugs"
+        >
+          <form
+            onSubmit={async (e) => {
+              e.preventDefault();
+              // Correctly type the event target to access form elements
+              const form = e.target as HTMLFormElement;
+              const bugDescriptionElement = form.elements.namedItem(
+                "bugDescription"
+              ) as HTMLTextAreaElement;
+
+              if (
+                session &&
+                session.user &&
+                session.user.email &&
+                bugDescriptionElement &&
+                bugDescriptionElement.value
+              ) {
+                // Assuming that session.user.email is defined based on the guarded clause above
+                await addBugReport(
+                  session.user.email,
+                  bugDescriptionElement.value
+                );
+                alert("Bug report submitted successfully!");
+                setReportBugModalOpen(false); // Close the modal
+              } else {
+                alert("An unexpected error occurred."); // Handle the unexpected case
+              }
+            }}
+          >
+            <textarea
+              className={styles.textareaField}
+              placeholder="Describe the bug..."
+              name="bugDescription"
+            ></textarea>
+            <div className={styles.formButtons}>
+              <button type="submit" className={styles.submitButton}>
+                Submit
+              </button>
+              <button
+                type="button"
+                className={styles.cancelButton}
+                onClick={() => setReportBugModalOpen(false)}
               >
                 Cancel
               </button>
