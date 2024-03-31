bae9515a287546d9c050b90041046c0deeedfffa
Zachary Vivian <161660633+zacharycvivian@users.noreply.github.com>
2024-03-31 15:01:39 -0500

    Testing head

--- src/app/about/metadata.tsx
+++ src/app/about/metadata.tsx
@@ -1,27 +1,17 @@
-import { useEffect } from "react";
+import Head from 'next/head';
 
 interface MetaProps {
-    title?: string;
-    description?: string;
-  }
-  
+  title?: string;
+  description?: string;
+}
 
-  const Meta: React.FC<MetaProps> = ({ title, description }) => {
-    useEffect(() => {
-    if (title) {
-      document.title = title;
-    }
-
-    let metaDescription = document.querySelector("meta[name='description']");
-    if (!metaDescription) {
-      metaDescription = document.createElement("meta");
-      metaDescription.setAttribute("name", "description");
-      document.getElementsByTagName("head")[0].appendChild(metaDescription);
-    }
-    metaDescription.setAttribute("content", description || "");
-  }, [title, description]);
-
-  return null;
+const Meta: React.FC<MetaProps> = ({ title, description }) => {
+  return (
+    <Head>
+      {title && <title>{title}</title>}
+      {description && <meta name="description" content={description} />}
+    </Head>
+  );
 };
 
 export default Meta;
