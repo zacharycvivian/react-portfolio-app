de779da37e5c41eeb2f2a0bb40120628ab038027
Zachary Vivian <161660633+zacharycvivian@users.noreply.github.com>
2024-03-31 15:52:55 -0500

    Testing another method of metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,101 +16,98 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 import Autoplay from "embla-carousel-autoplay";
-import { useEffect } from "react";
-
-const usePageMeta = (title: string, description: string): void => {
-  const defaultTitle = "Home - Zachary Vivian's Personal Website";
-  const defaultDesc = "Welcome to my personal website! Explore my journey and projects in cybersecurity.";
-
-  useEffect(() => {
-      document.title = title || defaultTitle;
-      const metaDescription = document.querySelector("meta[name='description']");
-      if (metaDescription) {
-          metaDescription.setAttribute("content", description || defaultDesc);
-      }
-  }, [title, description, defaultTitle, defaultDesc]);
-};
+import Head from "next/head";
 
 export default function Home() {
   const { data: session } = useSession();
-  usePageMeta("Home - Zachary Vivian's Personal Website", "Explore Zachary Vivian's professional journey and projects in cybersecurity on this personal website.");
+
+  const pageTitle = "Home - Zachary Vivian's Personal Website";
+  const pageDescription =
+    "Explore Zachary Vivian's professional journey and projects in cybersecurity on this personal website.";
 
   return (
-    <div className={styles.layoutContainer}>
-      <div className={styles.logoContainer}>
-        <Image
-          src={Logo}
-          alt="Zach Vivian's Logo"
-          layout="fill"
-          objectFit="contain"
-        />
-      </div>
-      <div className="container flex justify-center align-middle">
-        <div className={styles.homeContainer}>
-          <h1 className={styles.welcomeMessage}>
-            <strong>Hello, {session?.user?.name || "Guest"}</strong>
-          </h1>
-          <p className={styles.instructions}>
-            Click the icon on the top left to learn more about me and download a
-            copy of my resume!
-          </p>
-          <div className={styles.infoContainer}>
-            <h2>
-              <strong>Welcome to my Personal Website!</strong>
-            </h2>
-            <p>
-              My name is Zachary Vivian, and I am set to graduate from the
-              University of Wisconsin-Platteville in May 2024, with a degree
-              specializing in Cybersecurity. This website serves as a digital
-              portfolio where you can delve into my professional journey,
-              discover more about my passions and projects, and understand the
-              skills I bring to the cybersecurity domain. If we've had the
-              opportunity to collaborate or if you're familiar with my work and
-              dedication, I warmly invite you to leave a testimonial. Your
-              support as a reference could greatly assist me in connecting with
-              future employers and opportunities.
+    <>
+      <Head>
+        <title>{pageTitle}</title>
+        <meta name="description" content={pageDescription} />
+        {/* You can include other metadata tags here as needed */}
+      </Head>
+      <div className={styles.layoutContainer}>
+        <div className={styles.logoContainer}>
+          <Image
+            src={Logo}
+            alt="Zach Vivian's Logo"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+        <div className="container flex justify-center align-middle">
+          <div className={styles.homeContainer}>
+            <h1 className={styles.welcomeMessage}>
+              <strong>Hello, {session?.user?.name || "Guest"}</strong>
+            </h1>
+            <p className={styles.instructions}>
+              Click the icon on the top left to learn more about me and download
+              a copy of my resume!
             </p>
+            <div className={styles.infoContainer}>
+              <h2>
+                <strong>Welcome to my Personal Website!</strong>
+              </h2>
+              <p>
+                My name is Zachary Vivian, and I am set to graduate from the
+                University of Wisconsin-Platteville in May 2024, with a degree
+                specializing in Cybersecurity. This website serves as a digital
+                portfolio where you can delve into my professional journey,
+                discover more about my passions and projects, and understand the
+                skills I bring to the cybersecurity domain. If we've had the
+                opportunity to collaborate or if you're familiar with my work
+                and dedication, I warmly invite you to leave a testimonial. Your
+                support as a reference could greatly assist me in connecting
+                with future employers and opportunities.
+              </p>
+            </div>
+            <Carousel
+              className={styles.carouselItem}
+              opts={{
+                align: "start",
+                loop: true,
+              }}
+              plugins={[
+                Autoplay({
+                  delay: 3000,
+                }),
+              ]}
+            >
+              <CarouselContent>
+                <CarouselItem className={styles.image}>
+                  <Image
+                    src={Mountains}
+                    alt="A photo of Zach Vivian at the Garden of the Gods overlooking Pike's Peak in Colorado Springs, Colorado "
+                  />
+                </CarouselItem>
+                <CarouselItem className={styles.image}>
+                  <Image
+                    src={Zach}
+                    alt="A picture of Zach Vivian on a hike near Fish Creek Falls in Steamboat Springs, Colorado"
+                  />
+                </CarouselItem>
+                <CarouselItem className={styles.image}>
+                  <Image
+                    src={Squad}
+                    alt="Zach Vivian and his buddies on a hike near Nederland, Colorado"
+                  />
+                </CarouselItem>
+                <CarouselItem className={styles.image}>
+                  <Image src={Turbo} alt="Image of Zach Vivian's dog, Turbo" />
+                </CarouselItem>
+              </CarouselContent>
+              <CarouselPrevious />
+              <CarouselNext />
+            </Carousel>
           </div>
-          <Carousel
-            className={styles.carouselItem}
-            opts={{
-              align: "start",
-              loop: true,
-            }}
-            plugins={[
-              Autoplay({
-                delay: 3000,
-              }),
-            ]}
-          >
-            <CarouselContent>
-              <CarouselItem className={styles.image}>
-                <Image
-                  src={Mountains}
-                  alt="A photo of Zach Vivian at the Garden of the Gods overlooking Pike's Peak in Colorado Springs, Colorado "
-                />
-              </CarouselItem>
-              <CarouselItem className={styles.image}>
-                <Image
-                  src={Zach}
-                  alt="A picture of Zach Vivian on a hike near Fish Creek Falls in Steamboat Springs, Colorado"
-                />
-              </CarouselItem>
-              <CarouselItem className={styles.image}>
-                <Image
-                  src={Squad}
-                  alt="Zach Vivian and his buddies on a hike near Nederland, Colorado"
-                />
-              </CarouselItem>
-              <CarouselItem className={styles.image}>
-                <Image src={Turbo} alt="Image of Zach Vivian's dog, Turbo" />
-              </CarouselItem>
-            </CarouselContent>
-            <CarouselPrevious />
-            <CarouselNext />
-          </Carousel>
         </div>
       </div>
-    </div>
+    </>
   );
 }
