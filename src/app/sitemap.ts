d23c1269abc7fec26400861194e688e624b57f02
Zachary Vivian <161660633+zacharycvivian@users.noreply.github.com>
2024-04-01 16:58:03 -0500

    Added sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,36 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://www.zacharycvivian.com/",
+    },
+    {
+      url: "https://www.zacharycvivian.com/about",
+    },
+    {
+      url: "https://www.zacharycvivian.com/contact",
+    },
+    {
+      url: "https://www.zacharycvivian.com/blog",
+    },
+    {
+      url: "https://www.zacharycvivian.com/blog/what-i-learned-developing-website-prompt-engineering",
+    },
+    {
+      url: "https://www.zacharycvivian.com/blog/how-future-cybersecurity-changes-using-artifical-intelligence",
+    },
+    {
+      url: "https://www.zacharycvivian.com/testimonials",
+    },
+    {
+      url: "https://www.zacharycvivian.com/cyberwordle",
+    },
+    {
+      url: "https://www.zacharycvivian.com/snake",
+    },
+    {
+      url: "https://www.zacharycvivian.com/pong",
+    },
+  ];
+}
