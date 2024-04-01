4ff1b9ab3c0b1df6a427f7cb0c10a6c751f9ed83
Zachary Vivian <161660633+zacharycvivian@users.noreply.github.com>
2024-04-01 17:34:17 -0500

    Robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://zacharycvivian.com/sitemap.xml",
+    sitemap: "https://www.zacharycvivian.com/sitemap.xml",
   };
 }
