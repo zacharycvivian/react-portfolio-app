7bac40645fafdc2c43eeabf46bb0190a4e0316c6
Zachary Vivian <161660633+zacharycvivian@users.noreply.github.com>
2024-04-22 17:40:08 -0500

    next/head doesn't work right. undid changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,6 @@ import {
 } from "@/components/ui/carousel";
 import Autoplay from "embla-carousel-autoplay";
 import React, { useState, useEffect } from "react";
-import Head from "next/head";
 import {
   collection,
   doc,
@@ -189,13 +188,6 @@ export default function Home() {
 
   return (
     <>
-      <Head>
-        <link rel="preload" href={Logo.src} as="image" />
-        <link rel="auto" href={Zach.src} as="image" />
-        <link rel="auto" href={Turbo.src} as="image" />
-        <link rel="auto" href={Squad.src} as="image" />
-        <link rel="auto" href={Mountains.src} as="image" />
-      </Head>
       <motion.button
         id="chatbotButton"
         className={styles.chatbotbutton}
